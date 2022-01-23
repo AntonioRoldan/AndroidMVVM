d492a58fd54af629b9749999571f7f0ade6ac343
AntonioRoldan <antonioroldan95@outlook.com>
2022-01-23 18:31:49 +0100

    Only UI code left

--- app/src/main/java/io/keepcoding/mvvmarchitecture/ui/FirstFragment.kt
+++ app/src/main/java/io/keepcoding/mvvmarchitecture/ui/FirstFragment.kt
@@ -7,13 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import io.keepcoding.mvvmarchitecture.utils.CustomViewModelFactory
 import io.keepcoding.mvvmarchitecture.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
+    private val viewModel: FragmentOrActivityViewModel by lazy {
+        val factory = CustomViewModelFactory(requireActivity().application)
+        ViewModelProvider(this, factory).get(FragmentOrActivityViewModel::class.java)
+    }
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
@@ -24,9 +32,24 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        //With kotlinx we replace by button_first.setOnClickListener {
+        // }
+        //view.findViewById<Button>(R.id.button_first).setOnClickListener {
+         //   findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        //}
+    }
+    private fun fetchData() {
+        //viewModel.fetchPOJORetrofitResponses
+    }
+    private fun setUpObservers() {
+        fetchData()
+        //viewModel.getPOJORetrofitResponses().observe(viewLifecycleOwner, Observer {
+        //   when(it.status) {
+        //      Status.SUCCESS -> {
+        //
+        //      }
+        //   }
+        //
+        // })
     }
 }
