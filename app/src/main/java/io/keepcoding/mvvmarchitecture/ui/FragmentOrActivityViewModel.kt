bb5ff6c129f41ef6e89875d923e79d088dca81e6
AntonioRoldan <antonioroldan95@outlook.com>
2022-01-23 14:35:28 +0100

    Almost done

--- app/src/main/java/io/keepcoding/mvvmarchitecture/ui/FragmentOrActivityViewModel.kt
+++ app/src/main/java/io/keepcoding/mvvmarchitecture/ui/FragmentOrActivityViewModel.kt
@@ -12,11 +12,29 @@ import io.keepcoding.mvvmarchitecture.repository.remote.ApiHelper
 import kotlinx.coroutines.launch
 
 class FragmentOrActivityViewModel(private val context: Application, private val apiHelper: ApiHelper, private val localHelper: LocalHelper) : ViewModel() {
-    // private val entities : MutableLiveData<Resource<List<EntityItemViewModel?>>>()
+    // private val POJORetrofitResponses : MutableLiveData<Resource<List<POJORetrofitResponseItemViewModel?>>>() to show it in a recycler view
 
-    fun fetchPojoRetrofitResponse() {
-        viewModelScope.launch {
-            // entities.postValue(Resource.loading(null))
-        }
-    }
+    // fun fetchPojoRetrofitResponse() {
+        // viewModelScope.launch {
+            // POJORetrofitResponses.postValue(Resource.loading(null))
+    //         try {
+    //            val response = async { apiHelper.getEvents() }
+    //            val POJORetrofitResponseItemViewModels: List<POJORetrofitResponseItemViewModel?>? = response.await().POJORetrofitResponses?.map {
+    //              it?.data?.let { data ->
+    //                  POJORetrofitResponseItemViewModel(data = data, title = it.title, description = it.description as String?)
+    //              }
+    //            }
+    //            POJORetrofitResponses.postValue(Resource.success(POJORetrofitResponseItemViewModels))
+    //         } catch(e: Exception) {
+    //              POJORetrofitResponses.postValue(Resource.error(e.localizedMessage!!, null))
+    //         }
+        // }
+    //  }
+
+    //
+    // fun getPOJORetrofitResponses(): LiveData<Resource<List<POJORetrofitResponseItemViewModel?>>> {
+    //      return POJORetrofitResponses
+    // }
+    //
+    //
 }
